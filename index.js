135804ee0acb6d0a8f0d655f327d6f34d82b1e0e
Jomar Banting <shadowlover.05@gmail.com>
2023-10-22 02:34:06 -0400

    First Commit

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ server.use('*', (req, res) => {
   res.json({ message: 'API is UP!' })
 });
 
-server.listen(9000, () => {
-  console.log("\n*** Server Running on http://localhost:9000 ***\n");
+const port = process.env.PORT || 9000;
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
